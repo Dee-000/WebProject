a972ef38404fea63b7f560e45554c4aff06867b8
Dee <dmelamed@gov.pe.ca>
2025-05-17 13:38:30 -0400

    new exclamations

--- tests/TC001.spec.js
+++ tests/TC001.spec.js
@@ -1,6 +1,6 @@
 import { test, expect } from '@playwright/test';
 
-test('TC001', async ({ page }) => {
+test('TC001!', async ({ page }) => {
   await page.goto('https://google.com/');
   const googleAbout = page.getByRole('link', { name: 'About' });
   await expect(googleAbout).toBeVisible()
